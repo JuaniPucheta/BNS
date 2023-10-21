cb003e90a4d78d526a71de6eee923dc67ed1827b
Camila Piñero <cmlpr9@gmail.com>
2023-10-21 16:41:48 -0300

    Ejercicio clase 38, agregar swagger al ejercicio 37

--- app.js
+++ app.js
@@ -1,13 +1,42 @@
-const express = require('express');
-const usuarioRoutes = require('./routes/usuarios.js');
+import express from "express";
+import usuarioRoutes from "./routes/usuarioRoutes.js";
+import swaggerUI from "swagger-ui-express";
+import swaggerJsDoc from "swagger-jsdoc";
+import { fileURLToPath } from "url";
+import path from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
+
 const port = process.env.PORT || 4000;
 
-app.use(express.json());
+const swaggerSpec = {
+	definition: {
+		openapi: "3.0.0",
+		info: {
+			title: "Node Swagger API",
+			version: "1.0.0",
+		},
+		servers: [
+			{
+				url: "http://localhost:4000",
+			},
+		],
+	},
+	apis: [`${path.join(__dirname, "./routes/*.js")}`],
+};
 
+app.use(express.json());
 app.use(usuarioRoutes);
 
+app.use(
+	"/swagger",
+	swaggerUI.serve,
+	swaggerUI.setup(swaggerJsDoc(swaggerSpec))
+);
+
 app.listen(port, () => {
-    console.log(`Escuchando en ${port}`)
-})
+	console.log(`Escuchando en ${port}`);
+});
--- routes/usuarioRoutes.js
+++ routes/usuarioRoutes.js
@@ -1,14 +1,147 @@
-const express = require('express');
-
+import express from "express";
 const router = express.Router();
+import usuarioControllers from "../controllers/usuarioControllers.js";
 
-const usuarioController = require('../controllers/usuarioControllers.js');
+/**
+ * @swagger
+ *  components:
+ *      schemas:
+ *          Usuarios:
+ *              type: object
+ *              properties:
+ *                  id:
+ *                      type: integer
+ *                  name:
+ *                      type: string
+ *                  age:
+ *                      type: integer
+ *              example:
+ *                  id: 1
+ *                  name: Juan
+ *                  age: 25
+ *
+ *
+ */
 
-//* Definimos las rutas de usuario
-router.get('/allusers', usuarioControllers.getUsers);
-router.get('/oneuser/:id', usuarioControllers.getUser);
-router.post('/create', usuarioControllers.createUser);
-router.patch('/update/:id', usuarioControllers.updateUser);
-router.delete('/delete/:id', usuarioControllers.deleteUser);
+/**
+ * @swagger
+ * /allusers:
+ *  get:
+ *    summary: Devuelve todos los usuarios
+ *    tags: [Usuarios]
+ *    responses:
+ *      200:
+ *        description: Todos los usuarios
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: array
+ *              items:
+ *                $ref: '#/components/schemas/Usuarios'
+ */
+router.get("/allusers", usuarioControllers.getUsers);
 
-module.exports = router;
+/**
+ * @swagger
+ * /oneuser/{id}:
+ *  get:
+ *   summary: Devuelve un solo usuario
+ *   tags: [Usuarios]
+ *   parameters:
+ *     - in: path
+ *       name: id
+ *       schema:
+ *         type: number
+ *       required: true
+ *       description: El id del usuario
+ *   responses:
+ *     200:
+ *       description: Usuario relacionado al id provisto
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             $ref: '#/components/schemas/Usuarios'
+ *     404:
+ *       description: Usuario no encontrado
+ */
+router.get("/oneuser/:id", usuarioControllers.getUser);
+
+/**
+ * @swagger
+ * /usuarios:
+ *  post:
+ *      summary: Crea un nuevo usuario
+ *      tags: [Usuarios]
+ *      requestBody:
+ *          required: true
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                      type: object
+ *                      $ref: '#/components/schemas/Usuarios'
+ *                      properties:
+ *                          nombre:
+ *                              type: string
+ *                          edad:
+ *                              type: integer
+ *                  example:
+ *                       nombre: Pepa
+ *                       edad: 23
+ *
+ *      responses:
+ *          200:
+ *              description: Nuevo usuario creado
+ */
+router.post("/create", usuarioControllers.createUser);
+
+/**
+ * @swagger
+ * /update/:id:
+ *  put:
+ *      summary: Actualiza un usuario
+ *      tags: [Usuarios]
+ *      requestBody:
+ *          required: true
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                      type: object
+ *                      $ref: '#/components/schemas/Usuarios'
+ *                      properties:
+ *                          nombre:
+ *                              type: string
+ *                          edad:
+ *                              type: integer
+ *                  example:
+ *                       nombre: Pepita
+ *                       edad: 25
+ *
+ *      responses:
+ *          200:
+ *              description: Usuario actualizado
+ */
+router.patch("/update/:id", usuarioControllers.updateUser);
+
+/**
+ * @swagger
+ * /delete/{id}:
+ *  delete:
+ *   summary: Borra un usuario
+ *   tags: [Usuarios]
+ *   parameters:
+ *     - in: path
+ *       name: id
+ *       schema:
+ *         type: number
+ *       required: true
+ *       description: El id del usuario
+ *   responses:
+ *     204:
+ *       description: Usuario borrado
+ *     404:
+ *        description: Usuario no encontrado
+ */
+router.delete("/delete/:id", usuarioControllers.deleteUser);
+
+export default router;
